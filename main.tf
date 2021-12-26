52c21a82132390c75e8842eeb1d21d8f9e35beae
sk-kishtwar <96495361+sk-kishtwar@users.noreply.github.com>
2021-12-26 13:05:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,17 @@
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
 
-resource "google_compute_network" "vpc_networke" {
-  project = "{{avid-task-325512}}"
-  name                    = "vpc-networke"
-  auto_create_subnetworks = true
-  mtu                     = 1460
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
 }
