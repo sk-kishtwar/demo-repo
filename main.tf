88dd77ad9f102f4d2fc697bd43c194192fb1bbc1
sk-kishtwar <96495361+sk-kishtwar@users.noreply.github.com>
2021-12-26 12:56:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
+
 resource "google_compute_network" "vpc_networke" {
+  project = "{{avid-task-325512}}"
   name                    = "vpc-networke"
   auto_create_subnetworks = true
   mtu                     = 1460
